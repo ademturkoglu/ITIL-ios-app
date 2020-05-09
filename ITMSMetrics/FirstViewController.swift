05ac894843593b8547c4381024998377b578774d
aturkoglu <adem.turkoglu@hotmail.com>
2020-05-09 23:33:07 +0300

    Initial Commit

---
+++ ITMSMetrics/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ITMSMetrics
+//
+//  Created by ademturkoglu on 9.05.2020.
+//  Copyright © 2020 ademturkoglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ITMSMetrics/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ITMSMetrics
+//
+//  Created by ademturkoglu on 9.05.2020.
+//  Copyright © 2020 ademturkoglu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
